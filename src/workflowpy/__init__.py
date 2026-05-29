052f2570e940e5ba790f3456119f6fb591d07dc1
dalmijn <brencodeert@outlook.com>
2026-05-29 10:14:20 +0200

    Expose more

--- src/workflowpy/__init__.py
+++ src/workflowpy/__init__.py
@@ -1,3 +1,10 @@
 """Library template."""
 
 from .version import __version__
+from .workflow import Wildcards, Workflow, WorkflowConfig
+
+__all__ = [
+    "Wildcards",
+    "Workflow",
+    "WorkflowConfig",
+]
